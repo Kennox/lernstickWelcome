20f2f9ce1cc5575c0635390bfa29e09159c8a39a
Gyssim <simon.gysin@students.fhnw.ch>
2016-11-28 15:15:25 +0100

    added junit test class

---
+++ test/ch.fhnw/lernstickWelcomefx/model/LernstickWelcomeTest.java
@@ -0,0 +1,14 @@
+package ch.fhnw.lernstickWelcomefx.model;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 28.11.16.
+ */
+public class LernstickWelcomeTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+}
